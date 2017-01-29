ce62a72da25d032ac5c7042711c248f9e636a50c
saji-spoon <tkgkmny0707@hotmail.co.jp>
2017-01-30 04:48:54 +0900

    Writes "list" as C++.

---
+++ list.cpp
@@ -0,0 +1,147 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<vector>
+extern "C"{
+#include"SFMT/SFMT.h"
+#include"util.h"
+}
+#include"RNGData.hpp"
+#include<iostream>
+#include<fstream>
+#include<sstream>
+
+void saveFile(const std::vector<RandData>& rd)
+{
+	std::ofstream ofs("./lists/list.csv");
+        
+	ofs << "Consumed, Tick, VPlace, IV, Nature\n";
+
+        for(int i=0; i<rd.size(); ++i)
+        {
+		ofs << i << "," << rd[i] << "\n";
+        }
+}
+
+void saveFile(const std::vector<PokeStatus>& pkst)
+{
+	std::ofstream ofs("./lists/indivList.csv");
+        
+	
+	ofs << "Consumed,H,A,B,C,D,S,Nature\n";
+
+	for (int i=0; i<pkst.size(); ++i)
+	{
+		ofs << i << "," << pkst[i] << "\n";
+	}
+
+
+	
+}
+
+std::vector<RandData> getRandDataList(uint32_t seed, int searchN)
+{	
+        std::vector<RandData> rd(searchN);
+
+        sfmt_t sfmt;
+        
+        sfmt_init_gen_rand(&sfmt, seed);
+
+        //416 frame advances when the game starts.
+        for(int i=0; i<417; ++i)
+        {
+                sfmt_genrand_uint64(&sfmt);
+        }
+        
+
+        for(int i=0; i<searchN; ++i)
+        {
+                uint64_t result = sfmt_genrand_uint64(&sfmt);
+                rd[i].tick = result % 17;
+                rd[i].vp = result % 6;
+                rd[i].iv = result % 32;
+                rd[i].nature = result % 25;
+        }
+
+	return rd;
+}
+
+std::vector<PokeStatus> getPokeStatusList(const std::vector<RandData>& rd, int searchN)
+{
+	std::vector<PokeStatus> pkst(searchN);
+
+        for(int i=0; i<searchN; ++i)
+        {
+                //fprintf(indivFp, "%d,", i);
+                int state = 0;
+                for(int j=0; i+j < searchN; ++j)
+                {
+                        switch(state)
+                        {
+                                case 0: case 1: case 2:
+                                if(pkst[i].iv[rd[i+j].vp] != 31)
+                                {
+                                        pkst[i].iv[rd[i+j].vp] = 31;
+                                        ++state;
+                                }
+                                break;
+                                case 3:case 4: case 5:
+                                for(int h=0; h<6; ++h)
+                                {
+                                        if(pkst[i].iv[h] == -1)
+                                        {
+                                                pkst[i].iv[h] = rd[i+j].iv;
+                                                ++state;
+                                                break;
+                                        }
+                                        
+                                }
+                                break;
+                                case 6:
+                                for(int h=0; h<6; ++h)
+                                {
+                                        pkst[i].iv[h];//fprintf(indivFp, "%d,", pkst[i].iv[h]);
+                                }
+                                pkst[i].nature = rd[i+j].nature; //fprintf(indivFp, "N/A"/*, rd[i+j].nature*/);
+                                ++state;
+                                break;
+                        }
+                        
+                        if(state==7)
+                        {
+                                break;
+                        }
+                }
+                //fprintf(indivFp, "\n");
+        }
+	return pkst;
+	
+}
+int main(int argc, char* argv[])
+{
+        if(argc < 2 || strlen(argv[1]) < 8)
+        {
+                printf("Usage: %s 1234FEDC\n", argv[0]);
+                printf("Seed(32 bit) is required.\n");
+                return 1;
+        }
+
+	uint32_t seed;
+
+	std::stringstream ss;
+	ss << argv[1];
+	ss >> std::hex >> seed;
+
+	auto rd = getRandDataList(seed, 1000);
+	saveFile(rd);
+
+	auto pkst = getPokeStatusList(rd, rd.size());
+	saveFile(pkst);
+
+		
+        
+        printf("Lists created. See ./lists/\n");
+
+
+
+        return 0;
+}
--- makefile
+++ makefile
@@ -1,9 +1,12 @@
 CC := gcc
-CFLAGS := -O3 -std=c11 
+CXX := g++
+CFLAGS := -O3 -std=c11
+CXXFLAGS := -O3 -std=c++11
 SFMT_USE := -msse2 -fno-strict-aliasing -DSFMT_MEXP=19937 -DHAVE_SSE2=1 -I./SFMT
 
 COMPILE :=$(CC) $(CFLAGS) -c
-SFMT_USE_COMPILE =$(CC) $(CFLAGS) $(SFMT_USE) -c
+SFMT_USE_COMPILE :=$(CC) $(CFLAGS) $(SFMT_USE) -c
+SFMT_USE_COMPILE_CXX := $(CXX) $(CXXFLAGS) $(SFMT_USE) -c 
 
 default:db-search db-create tickList list
 
@@ -17,13 +20,16 @@ db-create:db-create.o
 tickList:tickList.o
 
 list:list.o
-
+	$(CXX) $^ -o $@
+        
 #Who use util/tickSeed/SFMT
 db-create db-search: tickSeed.o
 
 test-tick10 db-create db-search tickList list:util.o
 
 test-tick10 db-create tickList list:SFMT.o
+
+list:RNGData.o
 
 #Header file
 util.o:util.h
@@ -32,9 +38,14 @@ tickSeed.o:tickSeed.h
 
 SFMT.o:SFMT/SFMT.h
 
+RNGData.o:RNGData.hpp
+
 #Default compile(Use SFMT)
 %.o:%.c
 	$(SFMT_USE_COMPILE) $<
+%.o:%.cpp
+	$(SFMT_USE_COMPILE_CXX) $<
+	
 
 #Compile for Non-SFMT
 db-search.o:db-search.c
